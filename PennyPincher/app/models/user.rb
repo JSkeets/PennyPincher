054ef1ed019edd7e3908fa03118cc43eacaceff7
Jesse Skeets <jesseskeets@gmail.com>
2017-11-21 23:21:21 -0800

    Added email validation

--- PennyPincher/app/models/user.rb
+++ PennyPincher/app/models/user.rb
@@ -14,15 +14,17 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
-
+require 'valid_email'
 class User < ApplicationRecord
+  include ActiveModel::Validations
   validates :username, :session_token, :password_digest, presence:true
   validates :username, :session_token, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
+  validates :email, :presence => true, :email => true
 
   after_initialize :ensure_session_token
 
-  attr_reader :password
+  attr_reader :password,:email
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)

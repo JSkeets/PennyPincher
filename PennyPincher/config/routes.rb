3c99bff4759634e4bf73a0c2260aac622a178ef4
Jesse Skeets <jesseskeets@gmail.com>
2017-12-05 14:16:57 -0800

    Twitter API request works from backend, renders json to frontend

--- PennyPincher/config/routes.rb
+++ PennyPincher/config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create,:index,:show]
     resource :session, only: [:create,:destroy]
-    
   end
   resources :stocks, only: [:index,:show]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-
+  resources :tweets, only: [:show]
+  resources :password_resets,only: [:new, :create, :edit, :update]
 namespace :api  do
 resources :users do
     member do
